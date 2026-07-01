4b8db01614d67e1594adfd941c2ccbe0b923847a
Enlyl <sergeybasal@gmail.com>
2026-07-01 15:35:45 +0300

    Bump service worker cache to v2 to fix stale preloader

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 self.addEventListener('install',()=>self.skipWaiting());
 
-self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!=='dsml-v2').map(k=>caches.delete(k)))).then(()=>clients.claim())));
 
 self.addEventListener('fetch',e=>{
   e.respondWith(
-    caches.open('dsml-v1').then(c=>c.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+    caches.open('dsml-v2').then(c=>c.match(e.request).then(r=>r||fetch(e.request).then(res=>{
       if(res.ok&&res.type==='basic')c.put(e.request,res.clone());
       return res;
     }).catch(()=>new Response('Offline',{status:503}))))
